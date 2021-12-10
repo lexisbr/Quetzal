7581a92f687e509d9f24705120bc2a6c56118505
Alejandro Barrios <josebarriosrodas@gmail.com>
2021-12-09 23:52:30 -0600

    Execute instructions

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let grammar = require("./Gramatica/grammar.js")
+const AST  = require("./AST/AST.js");
+const Entorno = require("./AST/Entorno.js"); 
+const Instruccion =  require("./Interfaces/Instruccion.js");
+
+const grammar = require("./Gramatica/grammar.js")
+
 
 if (typeof window !== 'undefined'){
     window.parseExternal = function(input){ 
-        return grammar.parse(input);
+        const instrucciones = grammar.parse(input);
+        const ast = new AST.AST(instrucciones);
+        const entornoGlobal = new Entorno.Entorno(null);
+        instrucciones.forEach(function (element) {
+            element.ejecutar(entornoGlobal, ast);
+        });
     }
 }
+
